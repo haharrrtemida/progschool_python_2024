dc9dd15e761f7fc9b6d25cb4ab1101bc6d4148d3
Arrrtemida <artemlenin96@gmail.com>
2024-03-06 23:15:31 +0500

    added files from lesson 01

---
+++ Lesson 01/hello user.py
@@ -0,0 +1,21 @@
+# переменные
+
+# Переменная – ячейка для хранения данных
+
+# Шаблон создания
+# название = значение
+first_name = "Никодим"
+second_name = 'Петренко'
+age = 45
+weight = 99.5
+
+# str - строка
+# int – целое число
+# float – числа с плавающей точкой
+
+first_name = input("What's your name: ")
+print(f"Hello, {first_name}!")
+print("Hello" + first_name + "!")
+print("Hello", first_name, "!", sep = " :) ")
+
+input("Press Enter to exit...")
