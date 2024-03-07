5854f71c065c23c5a14afd864bb33b96848aad00
Arrrtemida <artemlenin96@gmail.com>
2024-03-07 21:50:45 +0500

    added files from lesson 02

---
+++ Lesson 02/focus.py
@@ -0,0 +1,20 @@
+first = input("a: ")
+second = input("b: ")
+
+# конкатенация строк (строки слипнутся)
+print(f"Sum: {first + second}")
+
+
+# 3.14 => 3
+# "3.14" => 3.14
+# "3" => 3
+# "3s" => Error
+
+# приведение типов
+# тип(что_переводим)
+
+first = int(input("a: "))
+second = int(input("b: "))
+
+# сумма двух чисел
+print(f"Sum:\n{first + second}")
---
+++ Lesson 02/operations.py
@@ -0,0 +1,22 @@
+a = 8
+b = 3
+
+print(a + b)    # 11
+print(a - b)    # 5
+print(a * b)    # 24
+print(a / b)    # 2,66
+print(a // b)   # 2
+print(a % b)    # 2
+print(a ** b)   # 512
+pow(2, 10)      # 512
+
+print(hex(458))
+print(bin(458))
+print(oct(458))
+
+
+print(5 / 2)    # 2.5
+print(5 // 2)   # 2
+
+print(3.14 / 2)    # 1.57
+print(3.14 // 2)   # 1.0
